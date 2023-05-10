42a10525d75eaf99d48b3eba4bf625e3768f64f1
firas1awadallah <firasawa@gmail.com>
2023-05-10 11:57:43 +0000

    solve

---
+++ README.md
@@ -0,0 +1,16 @@
+# LAB - Class 04
+
+## Project: pythonic-garage-band
+
+## Author: Firas awadallah
+
+### How to initialize/run your application (where applicable)
+python pythonic_garage_band/band.py
+
+### Tests
+* How do you run tests? 
+pytest
+* Any tests of note?
+No
+* Describe any tests that you did not complete, skipped, etc
+All Completed
---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,90 @@
+class Musician:
+    def __init__(self, name):
+        self.name = name
+
+
+class Guitarist(Musician):
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return "guitar"
+
+    def play_solo(self):
+        return "face melting guitar solo"
+
+
+class Bassist(Musician):
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return "bass"
+
+    def play_solo(self):
+        return "bom bom buh bom"
+
+
+class Drummer(Musician):
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return "drums"
+
+    def play_solo(self):
+        return "rattle boom crash"
+
+
+class Band:
+    instances = []
+
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        self.__class__.instances.append(self)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"Band instance. Name = {self.name}"
+
+    def play_solos(self):
+        return [member.play_solo() for member in self.members]
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+
+
+
+
+if __name__ == "__main__":
+    joan = Guitarist("Joan Jett")
+    sheila = Drummer("Sheila E.")
+    meshell = Bassist("Meshell Ndegeocello")
+
+    print(str(joan)) 
+    print(repr(joan))  
+    print(str(sheila))  
+    print(repr(sheila))  
+    print(str(meshell))  
+    print(repr(meshell))  
+
+    print(joan.get_instrument())  
+    print(sheila.get_instrument())  
+    print(meshell.get_instrument())  
+
+    print(joan.play_solo())  
+    print(sheila.play_solo())  
+    print(meshell.play_solo()) 
